afdbccbc5f6229ac2a33c94d31d5fee6acac2203
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-06-12 12:13:53 +0530

    test api

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
-export const host = "https://hnm-inc.herokuapp.com";
+// export const host = "https://hnm-inc.herokuapp.com";
+
+export const host = "http://10.0.2.2:3000";
 
 export const apiKey = "rzp_test_wIjrjqqlDLfHKq";
