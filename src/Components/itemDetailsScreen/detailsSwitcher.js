649d841311ed9aacd37d47157c1e79317a982f59
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-06-18 17:19:39 +0530

    changed Details switcher layout

--- src/Components/itemDetailsScreen/detailsSwitcher.js
+++ src/Components/itemDetailsScreen/detailsSwitcher.js
@@ -4,192 +4,137 @@
  */
 
 import React, { Component } from "react";
-import {
-  View,
-  StyleSheet,
-  TouchableNativeFeedback,
-  Text,
-  ScrollView,
-  Dimensions,
-} from "react-native";
-import { DataTable, Divider } from "react-native-paper";
+import { View, StyleSheet, Dimensions } from "react-native";
+import { DataTable, Divider, Text } from "react-native-paper";
 
 const screenWidth = Math.round(Dimensions.get("window").width);
 
 export default class DetailsSwitcher extends Component {
-  state = {
-    swipeCurrentPage: 0,
-  };
-
-  _snapPage = (event) => {
-    if (!event) return;
-
-    const xOffset = event.nativeEvent.contentOffset.x + 10;
-
-    const currentPage = Math.floor(xOffset / screenWidth);
-    this.setState(
-      (prevState) =>
-        currentPage != prevState.swipeCurrentPage && {
-          swipeCurrentPage: currentPage,
-        }
-    );
-  };
   render() {
     return (
       <View>
+        <View style={{ padding: 10 }}>
+          <Text style={{ fontSize: 17 }}>Product Information</Text>
+        </View>
+        <DataTable style={{ alignContent: "center" }}>
+          <DataTable.Row>
+            <DataTable.Cell>Weight</DataTable.Cell>
+            <DataTable.Cell>{`${this.props.item.weight} g`}</DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+            <DataTable.Cell>Size</DataTable.Cell>
+            <DataTable.Cell>{this.props.item.size}</DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+            <DataTable.Cell>Gender</DataTable.Cell>
+            <DataTable.Cell>{this.props.item.sex}</DataTable.Cell>
+          </DataTable.Row>
+        </DataTable>
         <View
           style={{
-            marginTop: 20,
             flexDirection: "row",
+            paddingHorizontal: 15,
+            borderBottomColor: "#eee",
+            borderBottomWidth: 1,
             alignItems: "center",
-            width: "100%",
-            //   borderWidth: 1,
-            height: 40,
+            paddingVertical: 10,
           }}
         >
-          <TouchableNativeFeedback
-            onPress={() => {
-              this.descriptionSwitcher.scrollTo({ x: 0 });
-            }}
-          >
-            <View
-              style={[
-                styles.descriptionSwitcherHeader,
-                this.state.swipeCurrentPage == 0 && {
-                  borderBottomColor: "#011627",
-                },
-              ]}
-            >
-              <Text>Product Information</Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback
-            onPress={() => {
-              this.descriptionSwitcher.scrollTo({
-                x: screenWidth,
-              });
-            }}
-          >
-            <View
-              style={[
-                styles.descriptionSwitcherHeader,
-                this.state.swipeCurrentPage == 1 && {
-                  borderBottomColor: "#011627",
-                },
-              ]}
-            >
-              <Text> Price Breakup</Text>
-            </View>
-          </TouchableNativeFeedback>
-        </View>
-        <ScrollView
-          onScroll={this._snapPage}
-          decelerationRate={10}
-          snapToInterval={screenWidth}
-          snapToAlignment={"center"}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          ref={(node) => (this.descriptionSwitcher = node)}
-        >
-          <View style={styles.card}>
-            <DataTable style={{ alignContent: "center", marginBottom: 20 }}>
-              <DataTable.Row>
-                <DataTable.Cell>Weight</DataTable.Cell>
-                <DataTable.Cell>{`${this.props.item.weight} g`}</DataTable.Cell>
-              </DataTable.Row>
-              <DataTable.Row>
-                <DataTable.Cell>Size</DataTable.Cell>
-                <DataTable.Cell>{this.props.item.size}</DataTable.Cell>
-              </DataTable.Row>
-              <DataTable.Row>
-                <DataTable.Cell>Gender</DataTable.Cell>
-                <DataTable.Cell>{this.props.item.sex}</DataTable.Cell>
-              </DataTable.Row>
-            </DataTable>
+          <View style={{ flex: 1 }}>
+            <Text>Materials</Text>
           </View>
-          <View style={styles.card}>
-            <DataTable theme={{ justifyContent: "center" }}>
-              <DataTable.Header style={{ justifyContent: "center" }}>
-                <DataTable.Title style={styles.tableHeaders}>
-                  COMPONENT
-                </DataTable.Title>
-                <DataTable.Title style={styles.tableHeaders}>
-                  RATE
-                </DataTable.Title>
-                <DataTable.Title style={styles.tableHeaders}>
-                  WEIGHT
-                </DataTable.Title>
-                <DataTable.Title style={styles.tableHeaders}>
-                  VALUE
-                </DataTable.Title>
-              </DataTable.Header>
-              {this.props.item.materials.map((material) => (
-                <DataTable.Row>
-                  <DataTable.Cell>{material.materialType.name}</DataTable.Cell>
+          <View style={{ flex: 1 }}>
+            {this.props.item.materials.map((material) => (
+              <Text>{`${material.materialType.name} - ${material.materialType.subtype}`}</Text>
+            ))}
+          </View>
+        </View>
 
-                  <DataTable.Cell>{`Rs.${material.materialType.price}`}</DataTable.Cell>
-                  <DataTable.Cell>{`${material.weight} g`}</DataTable.Cell>
-                  <DataTable.Cell>
-                    {`Rs.${(
-                      material.materialType.price * material.weight
-                    ).toFixed(2)}`}
-                  </DataTable.Cell>
-                </DataTable.Row>
-              ))}
+        <View>
+          <View style={{ padding: 10 }}>
+            <Text style={{ fontSize: 17 }}>Price Breakup</Text>
+          </View>
+          <DataTable theme={{ justifyContent: "center" }}>
+            <DataTable.Header style={{ justifyContent: "center" }}>
+              <DataTable.Title style={styles.tableHeaders}>
+                COMPONENT
+              </DataTable.Title>
+              <DataTable.Title style={styles.tableHeaders}>
+                RATE
+              </DataTable.Title>
+              <DataTable.Title style={styles.tableHeaders}>
+                WEIGHT
+              </DataTable.Title>
+              <DataTable.Title style={styles.tableHeaders}>
+                VALUE
+              </DataTable.Title>
+            </DataTable.Header>
+            {this.props.item.materials.map((material) => (
               <DataTable.Row>
-                <DataTable.Cell>Making</DataTable.Cell>
-                <DataTable.Cell>{`Rs.${this.props.item.makingCharges}`}</DataTable.Cell>
-                <DataTable.Cell>{`${this.props.item.weight} g`}</DataTable.Cell>
-                <DataTable.Cell>{`Rs.${(
-                  this.props.item.makingCharges * this.props.item.weight
-                ).toFixed(2)}`}</DataTable.Cell>
+                <DataTable.Cell>{material.materialType.name}</DataTable.Cell>
+
+                <DataTable.Cell>{`Rs.${material.materialType.price}`}</DataTable.Cell>
+                <DataTable.Cell>{`${material.weight} g`}</DataTable.Cell>
+                <DataTable.Cell>
+                  {`Rs.${(
+                    material.materialType.price * material.weight
+                  ).toFixed(2)}`}
+                </DataTable.Cell>
               </DataTable.Row>
-              <Divider style={{ backgroundColor: "red" }} />
+            ))}
+            <DataTable.Row>
+              <DataTable.Cell>Making</DataTable.Cell>
+              <DataTable.Cell>{`Rs.${this.props.item.makingCharges}`}</DataTable.Cell>
+              <DataTable.Cell>{`${this.props.item.weight} g`}</DataTable.Cell>
+              <DataTable.Cell>{`Rs.${(
+                this.props.item.makingCharges * this.props.item.weight
+              ).toFixed(2)}`}</DataTable.Cell>
+            </DataTable.Row>
+            <Divider style={{ backgroundColor: "red" }} />
+            <DataTable.Row>
+              <DataTable.Cell></DataTable.Cell>
+              <DataTable.Cell></DataTable.Cell>
+              <DataTable.Cell>
+                <Text style={{ fontWeight: "bold" }}>Sub Total</Text>
+              </DataTable.Cell>
+              <DataTable.Cell>
+                {" "}
+                <Text style={{ fontWeight: "bold" }}>
+                  {`Rs.${(+this.props.item.actualPrice).toFixed(2)}`}
+                </Text>
+              </DataTable.Cell>
+            </DataTable.Row>
+            {this.props.item.discount > 0 && (
               <DataTable.Row>
                 <DataTable.Cell></DataTable.Cell>
                 <DataTable.Cell></DataTable.Cell>
                 <DataTable.Cell>
-                  <Text style={{ fontWeight: "bold" }}>Sub Total</Text>
-                </DataTable.Cell>
-                <DataTable.Cell>
-                  {" "}
-                  <Text style={{ fontWeight: "bold" }}>
-                    {`Rs.${(+this.props.item.actualPrice).toFixed(2)}`}
-                  </Text>
-                </DataTable.Cell>
-              </DataTable.Row>
-              {this.props.item.discount > 0 && (
-                <DataTable.Row>
-                  <DataTable.Cell></DataTable.Cell>
-                  <DataTable.Cell></DataTable.Cell>
-                  <DataTable.Cell>
-                    <Text style={{ fontWeight: "bold" }}>(-)Discount</Text>
-                  </DataTable.Cell>
-                  <DataTable.Cell style={{ borderBottomWidth: 2 }}>
-                    <Text style={{ fontWeight: "bold" }}>
-                      {`Rs.${(
-                        this.props.item.actualPrice *
-                        (this.props.item.discount / 100)
-                      ).toFixed(2)}`}
-                    </Text>
-                  </DataTable.Cell>
-                </DataTable.Row>
-              )}
-              <DataTable.Row style={{ marginBottom: 20 }}>
-                <DataTable.Cell></DataTable.Cell>
-                <DataTable.Cell></DataTable.Cell>
-                <DataTable.Cell>
-                  <Text style={{ fontWeight: "bold" }}>Grand Total</Text>
+                  <Text style={{ fontWeight: "bold" }}>(-)Discount</Text>
                 </DataTable.Cell>
                 <DataTable.Cell style={{ borderBottomWidth: 2 }}>
                   <Text style={{ fontWeight: "bold" }}>
-                    {`Rs.${this.props.item.price.toFixed(2)}`}
+                    {`Rs.${(
+                      this.props.item.actualPrice *
+                      (this.props.item.discount / 100)
+                    ).toFixed(2)}`}
                   </Text>
                 </DataTable.Cell>
               </DataTable.Row>
-            </DataTable>
-          </View>
-        </ScrollView>
+            )}
+            <DataTable.Row style={{ marginBottom: 20 }}>
+              <DataTable.Cell></DataTable.Cell>
+              <DataTable.Cell></DataTable.Cell>
+              <DataTable.Cell>
+                <Text style={{ fontWeight: "bold" }}>Grand Total</Text>
+              </DataTable.Cell>
+              <DataTable.Cell style={{ borderBottomWidth: 2 }}>
+                <Text style={{ fontWeight: "bold" }}>
+                  {`Rs.${this.props.item.price.toFixed(2)}`}
+                </Text>
+              </DataTable.Cell>
+            </DataTable.Row>
+          </DataTable>
+        </View>
       </View>
     );
   }
@@ -199,14 +144,14 @@ const styles = StyleSheet.create({
   card: {
     width: screenWidth,
   },
-  descriptionSwitcherHeader: {
-    width: screenWidth / 2,
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100%",
-    borderBottomWidth: 1,
-    borderLeftWidth: 1,
-    borderColor: "#bbb",
-  },
+  // descriptionSwitcherHeader: {
+  //   width: screenWidth / 2,
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  //   height: "100%",
+  //   borderBottomWidth: 1,
+  //   borderLeftWidth: 1,
+  //   borderColor: "#bbb",
+  // },
   // tableHeaders: { justifyContent: "center" },
 });
