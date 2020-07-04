c02d5f21c1bc52f63274488fb7a2eddeae35e483
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-07-04 16:38:51 +0530

    customize UI done

--- src/screens/CustomJewellery.js
+++ src/screens/CustomJewellery.js
@@ -1,22 +1,10 @@
 import React, { Component } from "react";
 import { View } from "react-native";
 
-import { secondaryColor, primaryColor } from "../appStyles";
-
 import StepForm from "../Components/customJewellery/StepForm";
 
 export default class CustomJewellery extends Component {
   state = { selectedItemIndex: -1 };
-  ProgressStepsProps = {
-    activeStepIconBorderColor: secondaryColor,
-    progressBarColor: secondaryColor,
-    completedProgressBarColor: primaryColor,
-    completedStepIconColor: primaryColor,
-    activeLabelColor: secondaryColor,
-    completedLabelColor: primaryColor,
-    disabledStepIconColor: "#999",
-    disabledStepIconBorderColor: "#999",
-  };
 
   render() {
     return (
